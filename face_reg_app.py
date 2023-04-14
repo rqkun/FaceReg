b7473c733a64bf62646bc5e5335d4ce8cdb49401
rqkun <116709458+rqkun@users.noreply.github.com>
2023-04-14 17:45:17 +0700

    fliiped

--- face_reg_app.py
+++ face_reg_app.py
@@ -10,12 +10,20 @@ class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
         return av.VideoFrame.from_ndarray(img, format="bgr24")
+ 
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:]
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
 
 webrtc_ctx = webrtc_streamer(
-    key="WYH",
+    key="example", #WYH
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
-    video_processor_factory=VideoProcessor,
+    #video_processor_factory=VideoProcessor,
+    video_frame_callback=video_frame_callback,
     async_processing=True,
 )
