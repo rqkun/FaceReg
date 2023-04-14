de34dfe9abe6a8b71152a83434b18d673ae2c9ea
rqkun <116709458+rqkun@users.noreply.github.com>
2023-04-14 18:18:42 +0700

    change to TURN server

--- face_reg_app.py
+++ face_reg_app.py
@@ -3,7 +3,11 @@ import av
 import streamlit as st
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    {"iceServers": [{
+      "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+      "credential": "tE2DajzSJwnsSbc123",
+      "urls": "turn:global.turn.twilio.com:3478?transport=udp"
+    }]}
 )
 
 class VideoProcessor:
@@ -12,7 +16,7 @@ class VideoProcessor:
         return av.VideoFrame.from_ndarray(img, format="bgr24")
  
 webrtc_ctx = webrtc_streamer(
-    key="example", #WYH
+    key="WYH", #WYH
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
