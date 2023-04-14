0702c7252068f60f6d8d85d12c1724cbc34bd443
rqkun <116709458+rqkun@users.noreply.github.com>
2023-04-14 18:25:15 +0700

    restore to first change

--- face_reg_app.py
+++ face_reg_app.py
@@ -1,22 +1,17 @@
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 import av
-import streamlit as st
 
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{
-      "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-      "credential": "tE2DajzSJwnsSbc123",
-      "urls": "turn:global.turn.twilio.com:3478?transport=udp"
-    }]}
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
 class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
         return av.VideoFrame.from_ndarray(img, format="bgr24")
- 
+
 webrtc_ctx = webrtc_streamer(
-    key="WYH", #WYH
+    key="WYH",
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
