9621d842bf53690234faaa519b5647c379595931
rqkun <116709458+rqkun@users.noreply.github.com>
2023-04-14 17:38:43 +0700

    revert back to teach's code

--- face_reg_app.py
+++ face_reg_app.py
@@ -1,5 +1,6 @@
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 import av
+import streamlit as st
 
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
@@ -10,16 +11,11 @@ class VideoProcessor:
         img = frame.to_ndarray(format="bgr24")
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format="bgr24")
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
-
 webrtc_ctx = webrtc_streamer(
     key="WYH",
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
-    #video_processor_factory=VideoProcessor,
-    video_frame_callback=video_frame_callback,
+    video_processor_factory=VideoProcessor,
     async_processing=True,
 )
